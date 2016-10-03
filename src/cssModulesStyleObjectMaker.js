ae58a2935cd8de414d4765473dc8ca4c8a17f294
matteocng <matteocng@users.noreply.github.com>
2016-10-03 08:58:51 +0200

    chore(src): delete files before move to new directory

--- src/cssModulesStyleObjectMaker.js
+++
@@ -1,8 +0,0 @@
-// @flow
-import type { FlagIconOptionsType, ObjStringKeyValuesType } from './types/flow'
-
-
-export default (styles: ObjStringKeyValuesType,
-                options?: FlagIconOptionsType) : ObjStringKeyValuesType => { // eslint-disable-line arrow-body-style, max-len
-  return (options && options.themeStyles) ? { ...styles, ...options.themeStyles } : styles
-}
