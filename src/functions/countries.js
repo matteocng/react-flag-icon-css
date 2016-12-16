8acd57baf4d29829a3d0bb165e9281cf6c31e8d0
matteocng <matteocng@users.noreply.github.com>
2016-12-16 06:57:40 +0100

    chore: remove unused duplicate country codes file

--- src/functions/countries.js
+++ src/functions/countries.js
@@ -1,7 +1,7 @@
 import type { CountryType } from '../types/flow'
-import oCountries from '../static/countries'
+import oCountries from '../../static/countries.json'
 
 
-export const getCountries = () : Array<CountryType> => oCountries
-export const getCountryCodes = (): Array<string> =>
-  getCountries().map((o: CountryType) : string => o.code)
+export const getCountries = (): CountryType[] => oCountries
+export const getCountryCodes = (): FlagIconCodeType[] =>
+  getCountries().map((o: CountryType): FlagIconCodeType => o.code)
