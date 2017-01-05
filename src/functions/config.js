9067cfa9c279ff688c337dfda3fcaaa28ff7a4cc
matteocng <matteocng@users.noreply.github.com>
2017-01-05 23:07:17 +0100

    feat: print console message if no json-loader

--- src/functions/config.js
+++ src/functions/config.js
@@ -6,7 +6,7 @@ import { flagIconModulePath, flagIconClassesPrefix } from '../functions/constant
 import styles from '../styles'
 
 
-export const GetFlagIconModuleCountryCodes = ( // eslint-disable-line import/prefer-default-export
+export const GetFlagIconModuleCountryCodes = (
     modulePath: string = flagIconModulePath
 ): FlagIconCodeType[] =>
   fs.readdirSync(path.resolve(`${modulePath}/flags/4x3`)).reduce(
@@ -16,3 +16,22 @@ export const GetFlagIconModuleCountryCodes = ( // eslint-disable-line import/pre
     }
     , [] // reduce() second argument: initial value of retAr
   )
+
+export const ThrowNoJsonLoaderError = () => {
+  if (process.env.NODE_ENV !== 'production') { // UglifyJS strips this code out in production.
+    /* eslint-disable camelcase */
+    declare var __webpack_require__;
+    declare var __webpack_modules__;
+    const webpackSignatures = [typeof __webpack_require__, typeof __webpack_modules__]
+    /* eslint-enable camelcase */
+    const isWebpackOne = (webpackSignatures[0] !== 'undefined') || (webpackSignatures[1] !== 'undefined')
+
+    const message = 'react-flag-icon-css needs to import json files'
+    const suggestion = isWebpackOne ? ' update to Webpack 2 to load json out of the box or add ' +
+                                      'https://github.com/webpack/json-loader and this in your Webpack ' +
+                                      'configuration: { test: /\\.json$/, loader: "json-loader" }'
+                                    : ' add/configure the json loader or plugin for your module bundler.'
+
+    throw new Error(`${message}:${suggestion}`)
+  }
+}
--- src/functions/countries.js
+++ src/functions/countries.js
@@ -1,6 +1,15 @@
+import { ThrowNoJsonLoaderError } from './config'
 import type { CountryType } from '../types/flow'
 import oCountries from '../../static/countries.json'
 
+
+// Webpack 2 can require/import json out of the box, Webpack 1 and maybe other
+// bundlers cannot, so we warn the user. UglifyJS strips this code out in production.
+if (process.env.NODE_ENV !== 'production') {
+  if (!oCountries || Object.keys(oCountries).length === 0) {
+    ThrowNoJsonLoaderError()
+  }
+}
 
 export const getCountries = (): CountryType[] => oCountries
 export const getCountryCodes = (): FlagIconCodeType[] =>
