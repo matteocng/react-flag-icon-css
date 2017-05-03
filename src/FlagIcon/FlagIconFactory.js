8a80c09ae471b968a31c76296bbdeb3b6890a61d
matteocng <matteocng@users.noreply.github.com>
2017-05-03 08:14:03 +0200

    chore(package): update prettier to version 1.2.2

--- src/FlagIcon/FlagIconFactory.js
+++ src/FlagIcon/FlagIconFactory.js
@@ -49,8 +49,7 @@ const FlagIconFactory = (
             'https://github.com/gajus/react-css-modules#usage'
 
           // eslint-disable-next-line no-console
-          console.error(
-            `Warning: Detected configuration mismatch/error: [react-flag-icon-css] \
+          console.error(`Warning: Detected configuration mismatch/error: [react-flag-icon-css] \
 FlagIconFactory tried to build FlagIcon with the option 'useCssModules' set to true \
 (${userSetUseCssModules ? 'by you' : 'default'}), but it seems that your environment \
 is not loading the stylesheets in the appropriate way. Two possibilities: \
@@ -61,8 +60,7 @@ Try building FlagIcon like this: \n\n\tFlagIconFactory(React, { useCssModules: f
 is not correctly set up for CSS modules. If you are using Webpack, the key \
 modules to set up are 'style-loader', 'css-loader' and 'extract-text-webpack-plugin' \
 ('sass-loader' also comes into play if you are using SASS). \
-The link below may help you:\n\n\t${readmeModules}`,
-          )
+The link below may help you:\n\n\t${readmeModules}`)
         }
       }
     }
