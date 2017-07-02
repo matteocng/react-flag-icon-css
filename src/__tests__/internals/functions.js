67910cc62f7468ddb41e153a7205ddbf878a7bd3
matteocng <matteocng@users.noreply.github.com>
2017-07-02 22:59:53 +0200

    refactor: remove unused 'objectKeysApplyFn' and move 'diffArrays'

--- src/__tests__/internals/functions.js
+++ src/__tests__/internals/functions.js
@@ -83,3 +83,10 @@ export const restoreClassNamesInTree = (
   transformClassNames(modulesTree, className =>
     className.replace(new RegExp(cssModulePrefix, 'g'), ''),
   )
+
+export const diffArrays = <T>(arA: Array<T>, arB: Array<T>): Array<T> => {
+  const arrays = arA.length > arB.length ? [arA, arB] : [arB, arA]
+  return arrays[0].filter(
+    (newArElem: T): boolean => !arrays[1].includes(newArElem),
+  )
+}
