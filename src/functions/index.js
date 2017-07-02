fef095ce5e3e457f3a577f6ddad28c2dcb92a40f
matteocng <matteocng@users.noreply.github.com>
2017-07-02 22:59:53 +0200

    refactor: remove unused 'objectKeysApplyFn' and move 'diffArrays'

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,25 +1,6 @@
 // @flow
-import type classes from 'classnames'
-import type { KeyCallbackType } from '../types/flow'
-
 export { default as makeClassnames, makeClassesObject } from './classnames'
 export { default as makeFlagIconOptions } from './options'
 export { default as makeStyles } from './styles'
 export * as countries from './countries'
 export * as constants from './constants'
-
-export const objectKeysApplyFn = (obj: classes, fn: KeyCallbackType): classes =>
-  Object.keys(obj).reduce(
-    (rObj: classes, key: string, i: number): classes => ({
-      ...rObj,
-      [fn(key, i)]: obj[key],
-    }),
-    {}, // reduce() second argument: initial value of rObj
-  )
-
-export const diffArrays = <T>(arA: Array<T>, arB: Array<T>): Array<T> => {
-  const arrays = arA.length > arB.length ? [arA, arB] : [arB, arA]
-  return arrays[0].filter(
-    (newArElem: T): boolean => !arrays[1].includes(newArElem),
-  )
-}
