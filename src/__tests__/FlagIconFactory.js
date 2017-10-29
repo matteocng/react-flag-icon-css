fa8d61efc6937190cadc91ec2ade8fa045e03e74
matteocng <matteocng@users.noreply.github.com>
2017-10-29 16:28:55 +0100

    test: test that "import React from 'react'" works
    The user of this library may use "import React from 'react'" or the new
    syntax "import * as React from 'react'", we check that the former works
    too.
    
    Quoting from Flow documentation:
    "Again, if you import React with: import React from 'react' you will be able
    to access React.Component, React.createElement(), React.Children, and other
    JavaScript values. However, you will not be able to access React.Node,
    React.ChildrenArray or other Flow types. You will need to use a named type
    import like: import type {Node} from 'react' in addition to your default import."
    
    SEE: https://flow.org/en/docs/react/types/

--- src/__tests__/FlagIconFactory.js
+++ src/__tests__/FlagIconFactory.js
@@ -1,9 +1,20 @@
 // @flow
 import * as React from 'react' // eslint-disable-line import/no-duplicates
+import OldWayReact from 'react' // eslint-disable-line import/no-duplicates
+import render from 'react-test-renderer'
 import test from 'ava' // eslint-disable-line import/no-extraneous-dependencies
 import FlagIconFactory from '../'
 
 const ReactDefault = React.default
+
+// SEE: https://github.com/facebook/flow/tree/master/packages/flow-upgrade
+test('Works when React is imported in the "old way"', t => {
+  const FlagIcon = FlagIconFactory(OldWayReact)
+
+  t.notThrows(() => {
+    render.create(<FlagIcon code="it" />)
+  })
+})
 
 test('does not set FlagIcon.propTypes in production', t => {
   const previousEnv = process.env.NODE_ENV
