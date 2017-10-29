9df98d170d58274aa9796b6df3319c2a135f5adc
matteocng <matteocng@users.noreply.github.com>
2017-10-29 16:28:39 +0100

    chore: run 'flow-typed update'

--- flow-typed/npm/classnames_v2.x.x.js
+++ flow-typed/npm/classnames_v2.x.x.js
@@ -1,16 +1,23 @@
-// flow-typed signature: f18a1d7eaf96fce01718f217f06c838e
-// flow-typed version: 3c3f096590/classnames_v2.x.x/flow_>=v0.23.x
+// flow-typed signature: b391d71d029170a91ea550188224f180
+// flow-typed version: 2b8923a76c/classnames_v2.x.x/flow_>=v0.25.x
 
 type $npm$classnames$Classes =
-  string |
-  {[className: string]: * } |
-  Array<string> |
-  false |
-  void |
-  null
+  | string
+  | { [className: string]: * }
+  | false
+  | void
+  | null;
 
-declare module 'classnames' {
+declare module "classnames" {
   declare function exports(
-    ...classes: Array<$npm$classnames$Classes>
+    ...classes: Array<$npm$classnames$Classes | Array<$npm$classnames$Classes>>
   ): string;
+}
+
+declare module "classnames/bind" {
+  declare module.exports: $Exports<"classnames">;
+}
+
+declare module "classnames/dedupe" {
+  declare module.exports: $Exports<"classnames">;
 }
